37d92a834baf7173b334e36e6fcec860eb152007
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-02-15 16:16:21 +0200

    Add swiper hero

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -31,3 +31,22 @@
     // bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+
+const swiper = new Swiper('.hero-swiper', {
+  loop: true,
+  mousewheel: false,
+  grabCursor: true,
+  effect: 'cube',
+  cubeEffect: {
+    shadow: false,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+    pauseOnMouseEnter: true,
+  },
+});
